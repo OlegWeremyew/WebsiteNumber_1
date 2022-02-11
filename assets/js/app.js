4b5b5d38915d2e2bdd91389d951000a22a4f5c78
olegweremyew <olegweremey1994@mail.ru>
2022-02-11 23:12:44 +0300

    Updates

---
+++ assets/js/app.js
@@ -0,0 +1,49 @@
+$(function () {
+
+    let header = $("#header")
+    let introH = $("#intro").innerHeight()
+    let scrollOffset = $(window).scrollTop()
+
+
+    /* fixedHeader */
+    checkScroll(scrollOffset)
+
+    $(window).on("scroll", function () {
+        scrollOffset = $(this).scrollTop()
+        checkScroll(scrollOffset)
+    })
+
+    function checkScroll(scrollOffset) {
+        if (scrollOffset > +introH) {
+            header.addClass("fixed")
+        } else {
+            header.removeClass("fixed")
+        }
+    }
+
+    /* Smooth scroll */
+
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault()
+
+        let $this = $(this)
+        let blockId = $this.data("scroll")
+        let blockOffset = $(blockId).offset().top
+
+        $("#nav a").removeClass("active")
+        $this.addClass("active")
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500)
+    })
+
+    /* Menu nav toggle*/
+    $("#nav_toggle").on("click", function () {
+        event.preventDefault()
+
+        $(this).toggleClass("active")
+        $("#nav").toggleClass("active")
+
+    })
+})
